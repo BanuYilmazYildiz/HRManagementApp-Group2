892b2217d04b43c35dc446e0d162cd538102588e
BanuYilmazYildiz <banu.ylm@gmail.com>
2024-01-17 11:54:36 +0300

    Initial commit

---
+++ EmployeeMicroService/src/main/java/com/bilgeadam/repository/entity/Employee.java
@@ -0,0 +1,50 @@
+package com.bilgeadam.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Employee {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String secondName;
+    private String surname;
+    private String photo;
+    private Long birthDate; //TODO: date mi long mu?
+    private String birthplace;
+    private String tcNo;
+    private Date startDateToWork; //TODO: date mi long mu?
+    private Date resignationDate; //işten çıkış tarihi //TODO: date mi long mu?
+
+    @Builder.Default
+    private boolean isActive=true; //TODO: boolean mı? enum mu?
+    private String profession; //meslek
+    private String department;
+    private String company;
+    private String email; //ad.soyad@bilgeadamboost.com
+    private String address;
+    private String phone;
+    private Long salary; //TODO: long?
+
+
+
+
+
+
+
+
+
+}
