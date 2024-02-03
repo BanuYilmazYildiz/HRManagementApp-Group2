ef4fa3507ed310df5a6ae66bcaef27fbd2fd43e1
Sedat <sb.akdgn@gmail.com>
2024-02-03 04:09:09 +0300

    user ve manager microservice tum end point validationları eklendi

--- UserMicroService/src/main/java/com/bilgeadam/dto/request/RegisterRequestDto.java
+++ UserMicroService/src/main/java/com/bilgeadam/dto/request/RegisterRequestDto.java
@@ -1,10 +1,10 @@
 package com.bilgeadam.dto.request;
 
 import com.bilgeadam.utility.enums.ERole;
+import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,18 +25,38 @@ public class RegisterRequestDto {
     @NotEmpty(message = "Soyadı alanını boş bırakmayınız.")
     private String surname;
     private String photo;
+    @NotNull(message = "Dogum tarihi boş olamaz")
     private LocalDate birthday;
+    @NotEmpty(message = "Dogum yeri boş olamaz")
     private String birthplace;
+    @NotEmpty(message = "TC Kimlik Numarası boş olamaz")
+    @Pattern(regexp = "\\d+", message = "TC Kimlik Numarası sadece sayısal değerler içermelidir")
+    @Size(min = 11, max = 11, message = "TC Kimlik Numarası 11 karakter uzunluğunda olmalıdır")
+    @Column(unique = true, length = 11)
     private String tcNo;
+    @NotNull(message = "İşe başlama tarihi boş olamaz")
     private LocalDate startDateToWork;
-    private String profession;
+    private LocalDate resignationDate; //işten çıkış tarihi
+    @NotEmpty(message = "Meslek ismi boş olamaz")
+    private String profession; //meslek
+    @NotEmpty(message = "Departman ismi boş olamaz")
     private String department;
+    @NotEmpty(message = "Şirket ismi boş olamaz")
     private String company;
+    @Email
+    @NotEmpty(message = "E-mail kısmı boş olamaz")
     private String personalEmail;
+    @NotEmpty(message = "Adres boş olamaz")
     private String address;
+    @NotEmpty(message = "Telefon numarası boş olamaz")
+    @Pattern(regexp = "\\d+", message = "Telefon numarası sadece sayısal değerler içermelidir")
+    @Size(min = 11, max = 11, message = "Telefon numarası 11 karakter uzunluğunda olmalıdır")
+    @Column(unique = true, length = 11)
     private String phone;
+    @NotNull(message = "Maaş boş olamaz")
     private Double salary;
     @Enumerated(EnumType.STRING)
     private ERole role;
+
 
 }
